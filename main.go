bfc71d60aa31458bea5703af74306ad51883ca11
ichigo-dev <ichigo@web-freework.com>
2021-04-07 16:37:48 +0900

    create handlers

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
   "post-api/db"
+  "post-api/app/handlers/posts"
+
+  "net/http"
 )
 
 func main() {
   Db,_ := db.Db()
   Db.AutoMigrate(&db.Post{})
+
+  server := http.Server{
+    Addr: "127.0.0.1:8080",
+  }
+
+  http.HandleFunc("/posts", posts.Index)
+
+  server.ListenAndServe()
 }
